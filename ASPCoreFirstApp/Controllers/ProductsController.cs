b80b25fa9bdb399c1197717a114a3eca44c3be7f
Donnell Sample <dondon55.DS@Gmail.com>
2021-03-18 16:03:42 -0700

    update

--- ASPCoreFirstApp/Controllers/ProductsController.cs
+++ ASPCoreFirstApp/Controllers/ProductsController.cs
@@ -17,37 +17,57 @@ namespace ASPCoreFirstAPP.Controllers
         {
             repository = new ProductsDAO();
         }
+
+
         public IActionResult Index()
         {
             return View(repository.AllProducts());
            
         }
+
+
         public IActionResult SearchResults(string searchItem)
         {
             List<ProductModel> productsList = repository.SearchProducts(searchItem);
             return View("Index", productsList);
         }
+
+
         public IActionResult SearchForm()
         {
             return View();
         }
+
+
         public IActionResult ShowOneProduct(int id)
         {
             return View(repository.GetByProductId(id));
         }
+
+
         public IActionResult ShowEditForm(int id)
         {
             return View(repository.GetByProductId(id));
         }
+
+
         public IActionResult ProccessEdit(ProductModel product)
         {
             repository.Update(product);
             return View("Index",repository.AllProducts());
         }
+        public IActionResult ProcessEditReturn(ProductModel product)
+        {
+            repository.Update(product);
+            return View("Index", repository.AllProducts());
+        }
+
         public IActionResult DeleteById(ProductModel product)
         {
             repository.Delete(product);
             return View("Index", repository.AllProducts());
         }
+
+
     }
 }
--- ASPCoreFirstApp/wwwroot/js/site.js
+++ ASPCoreFirstApp/wwwroot/js/site.js
@@ -2,3 +2,56 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    //this button click fills the modal form iwth data and displays it.
+    $(document).on("click", ".edit-product-button", function (event) {
+        event.preventDefault();
+
+
+        //get the existing data from the current record 
+        var myProductId = $(this).data('id');
+        var myProductName = $(this).parent().find('.product-name').html();
+        var myProductPrice = $(this).parent().find('.product-price').html().replace("$", "");
+        var myProductDescription = $(this).parent().find('.product-description').html();
+
+
+        console.log("clicked button #" + myProductId);
+        console.log("name = " + myProductName);
+        console.log("price = " + myProductPrice);
+        console.log("desc = " + myProductDescription);
+
+        //fil in the data entry form 
+        $(".modal-dialog #Id").val(myProductId);
+        $(".modal-dialog #Name").val(myProductName);
+        $(".modal-dialog #Price").val(myProductPrice);
+        $(".modal-dialog #Description").val(myProductDescription);
+
+    });
+
+    //respond to the modal's save button
+    $("#save-product").click(function () {
+        //create a JSON object in order to submit the new changes to the controller
+        var Product = {
+            "Id": $('#Id').val(),
+            "Name": $('#Name').val(),
+            "Price": $('#Price').val(),
+            "Description": $('#Description').val()
+        };
+        console.log(Product)
+        doProductUpdate(Product)
+
+        function doProductUpdate(product){
+            $.ajax({
+                dataType: "json",
+                url: '/products/ProcessEditReturnOneItem',
+                data: product,
+                success: function (data) {
+                    //we should see a _productCard HTML printed to the console
+                    console.log(data);
+                    //target the correct product card by using its id number
+                    $("#card-number-" + Product.Id).html(data);
+                }
+            });
+        };
+    });
+});
